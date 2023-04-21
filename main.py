10c89b205836d7611790dc84dcf8e72e79cacc4f
innermost47 <anthony.charretier@gmail.com>
2023-04-21 15:24:55 +0200

    main.py seems ok

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ from dotenv import load_dotenv
 import html
 from requests.exceptions import JSONDecodeError
 from llama_cpp import Llama
+import unicodedata
 
 load_dotenv()
 
 news_api_key = os.environ.get("NEWS_API_KEY")
-pixabay_api_key = os.environ.get("PIXABAY_API_KEY")
+pexels_api_key = os.environ.get("PEXELS_API_KEY")
 my_domain_url = os.environ.get("MY_DOMAIN_URL")
 email = os.environ.get("EMAIL")
 password = os.environ.get("PASSWORD")
@@ -19,9 +20,10 @@ referer = os.environ.get("REFERER")
 your_model = os.environ.get("YOUR_MODEL")
 
 headers = {
-    'Referer': referer,
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0'
+    "Referer": referer,
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0",
 }
+
 
 class Sanitizer:
     @staticmethod
@@ -33,23 +35,7 @@ class Sanitizer:
     def convert_html_entities_to_characters(string):
         return html.unescape(string)
 
-model_path = your_model
-llm = Llama(model_path=model_path, n_threads=2)
 
-ins = '''### Instruction:
-{}
-### Response:
-'''
-
-def generate(instruction): 
-    result = ""
-    for x in llm(ins.format(instruction), stop=['### Instruction:', '### End'], stream=True):
-        result += x['choices'][0]['text']
-    res = ""
-    for response in result:
-        res += response
-    return res
-   
 categories = [
     "business",
     "entertainment",
@@ -59,6 +45,34 @@ categories = [
     "sports",
     "technology",
 ]
+
+llm = Llama(model_path=your_model, n_threads=4, n_ctx=4000)
+
+ins = """### Instruction:
+{}
+### Response:
+"""
+
+
+def generate(instruction):
+    result = ""
+    for x in llm(
+        ins.format(instruction),
+        stop=["### Instruction:", "### End"],
+        stream=True,
+        max_tokens=1200,
+    ):
+        result += x["choices"][0]["text"]
+    return result
+
+
+def clean_text(text):
+    return "".join(
+        c
+        for c in unicodedata.normalize("NFKD", text)
+        if unicodedata.category(c) != "Mn"
+    )
+
 
 def fetch_news(category):
     url = f"https://newsapi.org/v2/top-headlines?country=us&category={category}&apiKey={news_api_key}"
@@ -85,26 +99,26 @@ def fetch_news(category):
 
     return []
 
+
 def fetch_image_url(query):
-    url = f"https://pixabay.com/api/?key={pixabay_api_key}&q={urllib.parse.quote(query)}&image_type=photo"
+    pexels_headers = {"Authorization": pexels_api_key}
+    url = f"https://api.pexels.com/v1/search?query={urllib.parse.quote(query)}&per_page=1&page=1"
     try:
-        response = requests.get(url)
+        response = requests.get(url, headers=pexels_headers)
         response.raise_for_status()
 
         try:
-            data = response.json()
+            json_data = response.json()
+            image_url = json_data["photos"][0]["src"]["original"]
         except JSONDecodeError:
             print(f"Invalid JSON response from {url}")
             return None
+        return image_url
 
-        if data["total"] > 0:
-            random_image = random.choice(data["hits"])
-            return random_image["webformatURL"]                     
-        return None
-    
     except requests.exceptions.RequestException as e:
         print(f"An error occurred while making a request to {url}: {e}")
         return None
+
 
 def summarize_news(articles):
     text_to_summarize = " ".join(
@@ -115,29 +129,33 @@ def summarize_news(articles):
         ]
     )
 
-    prompt = (
-        f"Please provide a large summary of the following news: {text_to_summarize}"
-    )
+    prompt = f"Summurize these news in one text: {text_to_summarize}"
 
-    return generate(prompt)
+    response = generate(clean_text(prompt))
+    print(response)
+    return response
+
 
 def summarize_article(article):
-    prompt = (
-        f"Provide a short summary of the following news: {article}"
-    )
-    return generate(prompt)
+    prompt = f"Summurize: {article}"
+    response = generate(clean_text(prompt))
+    print(response)
+    return response
+
 
 def find_title(content):
-    prompt = f"Provide a concise title for the following article: {content}"
-    return generate(prompt)
+    prompt = f"Give a very concise title wich describe this: {content}"
+    response = generate(clean_text(prompt))
+    print(response)
+    return response
 
-def find_image_key_word(content):
-    prompt = f"Generate a single word that describe the most the following content : {content}"
-    return generate(prompt)
 
 def generate_email_response(username, message):
     prompt = f"You are autoGenius, a news blog writer, {username} sent you this email: {message} via your blog, answer him on a professional way"
-    return generate(prompt)
+    response = generate(clean_text(prompt))
+    print(response)
+    return response
+
 
 def generate_response(short_content, comments):
     prompt = f"[autoGenius]: '{short_content}'\n"
@@ -147,7 +165,17 @@ def generate_response(short_content, comments):
 
     prompt += "[autoGenius]: "
 
-    return generate(prompt)
+    response = generate(clean_text(prompt))
+    print(response)
+    return response
+
+
+def extract_keywords(content):
+    prompt = f"Summerize in only one word: {content}"
+    response = generate(clean_text(prompt))
+    print(response)
+    return response
+
 
 def send_to_api(category, title, content, image_url, short_content, sources, token):
     api_url = f"{my_domain_url}"
@@ -170,6 +198,7 @@ def send_to_api(category, title, content, image_url, short_content, sources, tok
     response = requests.post(api_url, data=data, headers=headers)
     return response.status_code
 
+
 def send_response(content, article_id, token):
     api_url = f"{my_domain_url}"
     data = {
@@ -181,6 +210,7 @@ def send_response(content, article_id, token):
 
     response = requests.post(api_url, data=data, headers=headers)
     return response.status_code
+
 
 def send_email(send_to, message, token):
     api_url = f"{my_domain_url}"
@@ -194,6 +224,7 @@ def send_email(send_to, message, token):
 
     response = requests.post(api_url, data=data, headers=headers)
     return response.status_code
+
 
 def login(email, password):
     api_url = f"{my_domain_url}"
@@ -209,6 +240,7 @@ def login(email, password):
         return response.json()["token"]
     return None
 
+
 def fetch_comments(token):
     api_url = f"{my_domain_url}?page=comments&token={token}"
 
@@ -217,9 +249,12 @@ def fetch_comments(token):
     if response.status_code == 200:
         return response.json()
     else:
-        print(f"Error fetching unanswered comments. Status code: {response.status_code}")
+        print(
+            f"Error fetching unanswered comments. Status code: {response.status_code}"
+        )
         return []
-    
+
+
 def fetch_emails(token):
     api_url = f"{my_domain_url}?page=email&token={token}"
 
@@ -230,16 +265,22 @@ def fetch_emails(token):
     else:
         print(f"Error fetching unanswered emails. Status code: {response.status_code}")
         return []
-    
+
+
 def set_email_as_answered(email_id):
     api_url = f"{my_domain_url}?page=email&id={email_id}&token={token}"
     response = requests.get(api_url, headers=headers)
 
     if response.status_code == 200:
-        print(f"Success set email {email_id} as answered. Status code: {response.status_code}")
+        print(
+            f"Success set email {email_id} as answered. Status code: {response.status_code}"
+        )
     else:
-        print(f"Error set email {email_id} as answered. Status code: {response.status_code}")
-    
+        print(
+            f"Error set email {email_id} as answered. Status code: {response.status_code}"
+        )
+
+
 def create_post(token):
     for post_attempts in range(3):
         random_category = random.choice(categories)
@@ -249,7 +290,7 @@ def create_post(token):
         for i, url in enumerate(sources):
             news_dict[f"news{i+1}"] = url
         sources_json = json.dumps(news_dict)
-        
+
         if news_articles:
             while True:
                 content = summarize_news(news_articles)
@@ -267,33 +308,34 @@ def create_post(token):
                     print("Failed to summarize article. Trying again later...")
                     continue
 
-                image_key_word = find_image_key_word(title)
-                if not image_key_word:
-                    print("Failed to find image key word.")
+                keywords = extract_keywords(content)
+                if not keywords:
+                    print("Failed to find keywords. Trying again later...")
                     continue
 
-                for image_attempts in range(3):
-                    image_url = fetch_image_url(image_key_word)
-                    if image_url:
-                        break
-                    print("Failed to fetch image url. Trying again later...")
-                    image_key_word = find_image_key_word(title)
+                image_url = fetch_image_url(keywords)
 
-                if not image_url:
-                    break
-
-                print("All variables are valid.")
-                status = send_to_api(random_category, title, content, image_url, short_content, sources_json, token)
+                print(
+                    title, image_url, short_content, sources_json, content, sep="\n\n"
+                )
+                status = send_to_api(
+                    random_category,
+                    title,
+                    content,
+                    image_url,
+                    short_content,
+                    sources_json,
+                    token,
+                )
                 print(f"Article submitted with status code {status}")
                 return True
 
-            if not image_url:
-                print("Failed to fetch image after 3 attempts. Trying a new post...")
         else:
             print("Failed to fetch news. Try again later.")
-    
+
     print("Failed to create a post after 3 attempts.")
     return False
+
 
 if __name__ == "__main__":
     token = login(email, password)
@@ -310,7 +352,7 @@ if __name__ == "__main__":
 
     unanswered_comments = fetch_comments(token)
 
-    if(unanswered_comments) :
+    if unanswered_comments:
         for article in unanswered_comments:
             article_id = article["id"]
             short_content = article["short_content"]
@@ -319,13 +361,17 @@ if __name__ == "__main__":
                 response = generate_response(short_content, comments)
                 if response:
                     status = send_response(response, article_id, token)
-                    print(f"Comments for article { article_id } replied with status code {status}")
+                    print(
+                        f"Comments for article { article_id } replied with status code {status}"
+                    )
                 else:
-                    print(f"Failed to generate response for comment of article { article_id }. Skipping.")
-                    
+                    print(
+                        f"Failed to generate response for comment of article { article_id }. Skipping."
+                    )
+
     unanswered_emails = fetch_emails(token)
-    
-    if(unanswered_emails):
+
+    if unanswered_emails:
         for unanswerd_email in unanswered_emails:
             email_id = unanswerd_email["id"]
             send_to = unanswerd_email["email_sender"]
@@ -334,10 +380,10 @@ if __name__ == "__main__":
             response = generate_email_response(sender_username, sender_message)
             if response:
                 status = send_email(send_to, response, token)
-                print(f"Response for email { email_id } replied with status code {status}")
+                print(
+                    f"Response for email { email_id } replied with status code {status}"
+                )
                 if status == 200:
                     set_email_as_answered(email_id)
             else:
                 print(f"Failed to generate response for email { email_id }. Skipping.")
-        
-             
