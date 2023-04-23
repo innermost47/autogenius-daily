f2e92b273c5cbbf419652cb6f8c18d49770bccc6
innermost47 <anthony.charretier@gmail.com>
2023-04-23 18:49:09 +0200

    update env.template.txt

--- main.py
+++ main.py
@@ -27,13 +27,6 @@ email = os.environ.get("EMAIL")
 password = os.environ.get("PASSWORD")
 your_model = os.environ.get("YOUR_MODEL")
 
-user_agents = [
-    "Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0",
-    "Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0",
-    "Mozilla/5.0 (X11; Linux x86_64; rv:95.0) Gecko/20100101 Firefox/95.0",
-]
-random_user_agent = random.choice(user_agents)
-
 headers = {
     "Referer": "http://www.google.com",
     "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:90.0) Gecko/20100101 Firefox/90.0",
